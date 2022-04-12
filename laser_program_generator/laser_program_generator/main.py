753e72a2fc8b7915526cffd1891481967283e897
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-04-12 10:27:05 -0500

    remove unneed callback

--- laser_program_generator/laser_program_generator/main.py
+++ laser_program_generator/laser_program_generator/main.py
@@ -4,15 +4,8 @@ import typer
 app = typer.Typer()
 
 
-@app.callback()
-def callback():
-    """
-    Awesome Laser Program Generator
-    """
-
-
 @app.command()
-def generate_program():
+def generate_program(pattern: str):
     """
     Given a desired output pattern, this command generates a program to be executed by the laser cutter
     """
