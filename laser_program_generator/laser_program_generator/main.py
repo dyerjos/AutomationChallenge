48a52215171977e92416e82ca82a56cc4aa7cb33
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-04-12 11:26:30 -0500

    plan input validation

--- laser_program_generator/laser_program_generator/main.py
+++ laser_program_generator/laser_program_generator/main.py
@@ -17,6 +17,11 @@ def generate_program(pattern: str):
     Given a desired output pattern as a string, this command generates a program to be executed by the laser cutter
     """
 
+    # * function to validate the input as being a valid string
+    # * function should also make sure that each row and column has the same number of elements
+    # * exit early if not valid input
+    # * might as well determine grid dimensions here as well
+
     typer.echo("Welcome!\n")
     typer.echo(
         "This program accepts a desired pattern as a string\n"
@@ -34,3 +39,6 @@ def generate_program(pattern: str):
     if not pattern_confirmed:
         typer.echo("Aborting due to unconfirmed output pattern")
         raise typer.Abort()
+
+    # * function to parse the pattern string
+    # * this function will determin cut mark coordinates and store them
