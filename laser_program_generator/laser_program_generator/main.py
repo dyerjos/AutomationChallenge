d4c1d170e57ff16a958ed765ed94395d6ffd3ccc
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-04-12 11:17:41 -0500

    work on receiving desired input string from user

--- laser_program_generator/laser_program_generator/main.py
+++ laser_program_generator/laser_program_generator/main.py
@@ -4,9 +4,33 @@ import typer
 app = typer.Typer()
 
 
+@app.callback()
+def main():
+    """
+    Laser program generator
+    """
+
+
 @app.command()
 def generate_program(pattern: str):
     """
-    Given a desired output pattern, this command generates a program to be executed by the laser cutter
+    Given a desired output pattern as a string, this command generates a program to be executed by the laser cutter
     """
-    typer.echo("Generating machine program!")
+
+    typer.echo("Welcome!\n")
+    typer.echo(
+        "This program accepts a desired pattern as a string\n"
+        "Input Example:\n"
+        """
+        ..XXX..
+        XX...XX
+        ..XXX..
+        \n"""
+    )
+
+    typer.echo(f"You provided the following pattern:\n {pattern}")
+
+    pattern_confirmed = typer.confirm("Is this your desired pattern?\n")
+    if not pattern_confirmed:
+        typer.echo("Aborting due to unconfirmed output pattern")
+        raise typer.Abort()
