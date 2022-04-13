7d4e772a00bd45fb0cf638d1ca11c257379855ef
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-04-12 20:53:11 -0500

    tests still fail due to a bug with Typer?

---
+++ laser_program_generator/tests/test_main.py
@@ -0,0 +1,22 @@
+from typer.testing import CliRunner
+
+from laser_program_generator import main
+
+runner = CliRunner()
+
+# TODO: figure out why tests can't run (AttributeError: module 'laser_program_generator.main' has no attribute '_add_completion')
+
+
+def test_app():
+    result = runner.invoke(
+        main,
+        [
+            """
+        ..XXX..
+        XX...XX
+        ..XXX..
+        """
+        ],
+    )
+
+    assert result.exit_code == 0
