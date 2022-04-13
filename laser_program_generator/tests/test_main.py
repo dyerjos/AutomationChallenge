4c91c6eb040bf045753c84c536508917dd78436c
Joshua Dyer <dyerjo@mail.gvsu.edu>
2022-04-13 05:58:01 -0500

    test unconfirmed output

--- laser_program_generator/tests/test_main.py
+++ laser_program_generator/tests/test_main.py
@@ -5,16 +5,40 @@ from laser_program_generator.main import app
 runner = CliRunner()
 
 
-test_pattern_one = """
+test_case_one = {
+    "pattern": """
     ..XXX..
     XX...XX
     ..XXX..
-    """
+    """,
+    "run_time": 21.952,
+}
+test_case_two = {
+    "pattern": """
+    ....X....
+    ..XXXXX..
+    .........
+    ..XXXXX..
+    ....X....
+    """,
+    "run_time": 29.728,
+}
+test_case_three = {
+    "pattern": """
+    ......XXX
+    ........X
+    X...X...X
+    X........
+    XXX......
+    """,
+    "run_time": 29.903,
+}
 
 
-def test_app():
-    result = runner.invoke(app, ["generate-program", test_pattern_one], input="y")
-    assert result.exit_code == 0
+def test_unconfirmed_output():
+    result = runner.invoke(app, ["generate-program", "x"], input="n")
+    assert result.exit_code == 1
+    assert "Aborting due to unconfirmed output pattern" in result.stdout
 
 
 # TODO: test all functions of main.py
